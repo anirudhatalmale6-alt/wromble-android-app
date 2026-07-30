d13563dcac58190ab13242aaaff0a6d15cf05410
Anirudha Talmale <anirudhatalmale6-alt@users.noreply.github.com>
2026-07-30 01:22:54 +0000

    Bump versionCode 40 / 2.5.0 (Play: code 29 already used)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.wromble.order"
         minSdk = 24
         targetSdk = 36
-        versionCode = 29
-        versionName = "2.4.1"
+        versionCode = 40
+        versionName = "2.5.0"
         vectorDrawables { useSupportLibrary = true }
     }
 
